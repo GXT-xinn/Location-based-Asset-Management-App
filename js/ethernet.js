1c27166005b18c1dd2dad63788bf4740b8b72342
Xintong Guan <xintong052499@gmail.com>
2022-01-20 18:44:57 +0000

    Changed the symbology of busstops and ethernet data based on the attributes

--- js/ethernet.js
+++ js/ethernet.js
@@ -1,24 +1,53 @@
-var ethernetLayer;
+var ethernetCables;
 
 
 function removeEthernetData() { 
  try { 
  alert("Ethernet data will be removed"); 
- mymap.removeLayer( ethernetLayer ); 
+ mymap.removeLayer( ethernetCables ); 
  } catch (err) { alert("Layer doesn’t exist :" + err); 
  } 
 } ;
 
-function getEthernetData() { 
- var layerURL = document.location.origin + "/app/data/ethernet.geojson"; 
- $.ajax({url: layerURL, crossDomain: true,success: function(result){ 
- console.log(result); // check that the data is correct 
- 
-// add the JSON layer onto the map - it will appear using the default icons 
+function getEthernetCables() { 
+var layerURL = document.location.origin + "/app/data/ethernet.geojson"; 
+ $.ajax({url: layerURL, dataType: 'json', success: function(result){ 
+ console.log(result); 
+ var style1 = { 
+ "color": "#ea3008", 
+ "weight": 10, 
+ "opacity": 0.65 
+ };
+ var style2 = { 
+ "color": "#08EA3E", 
+ "weight": 10, 
+ "opacity": 0.65 
+ };
+ var style3 = { 
+ "color": "#0811EA", 
+ "weight": 10, 
+ "opacity": 0.65 
+ };
  // load the geoJSON layer 
- ethernetLayer = L.geoJson(result).addTo(mymap); 
+ ethernetCables = L.geoJSON().addTo(mymap); 
+ ethernetCables.addData(result); 
+ // iterate over the lines and set style depending on district 
+ ethernetCables.eachLayer(function(layer) { 
+ console.log(layer); 
+ switch (layer.feature.properties.criticality) { 
+ case 2: 
+ layer.setStyle(style1); 
+ break; 
+ case 3: 
+ layer.setStyle(style2); 
+ break; 
+ default: 
+ layer.setStyle(style2); 
+ } 
+ });
  // change the map zoom so that all the data is shown 
- mymap.fitBounds(ethernetLayer.getBounds()); 
+ mymap.fitBounds(ethernetCables.getBounds()); 
  } // end of the inner function 
  }); // end of the ajax request 
-} ;// end of the getEarthquakeData function
+ 
+} // end of the function
