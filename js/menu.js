5b5097239940ba37cbc1e6367fcef6e29bc1f9ad
Xintong Guan <xintong052499@gmail.com>
2022-01-27 02:10:58 +0000

    Added file: menu.js and apply menu function into bootstrap html to raise alerts when clicks

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+
+function menu1() {
+	alert("menu number is 1 and menu is called by: "+ arguments.callee.name); 
+}
+
+function menu3() {
+	alert("Under channel 'Menu 2', menu is called by: "+ arguments.callee.name); 
+}
+
+function menu4() {
+	alert("Under channel 'Menu 2', menu is called by: "+ arguments.callee.name); 
+}
+
+function menu7() {
+	alert("Under channel 'Menu 5', menu is called by: "+ arguments.callee.name); 
+}
+
+
+function menu9() {
+	alert("Under channel 'Menu 8', menu is called by: "+ arguments.callee.name); 
+}
+
+function menu10() {
+	alert("Under channel 'Menu 8', menu is called by: "+ arguments.callee.name); 
+}
+
+
+function menu12() {
+	alert("Under channel 'Menu 11', menu is called by: "+ arguments.callee.name); 
+}
+function menu13() {
+	alert("Under channel 'Menu 11', menu is called by: "+ arguments.callee.name); 
+}
+function menu15() {
+	alert("Under channel 'Menu 14', menu is called by: "+ arguments.callee.name); 
+}
+function menu16() {
+	alert("Under channel 'Menu 14', menu is called by: "+ arguments.callee.name); 
+}
