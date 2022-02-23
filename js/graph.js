35e3591625bc19f83502a06c96e80f07d3a0ae48
Xintong Guan <xintong052499@gmail.com>
2022-02-23 18:06:42 +0000

    Fixed the typo issue in graph.js

--- js/graph.js
+++ js/graph.js
@@ -3,7 +3,7 @@ document.getElementById("graphWrapper").style.top = "-9999px";
 }
 
 
-function showGraph {
+function showGraph() {
 document.getElementById("graphWrapper").style.top = "300px";
 // the code to generate the graph goes here
 document.getElementById("graphWrapper").style.top="15%";
@@ -12,7 +12,5 @@ var widtha = document.getElementById("graphWrapper").offsetWidth;
 var heighta = document.getElementById("graphWrapper").offsetHeight;
 console.log(widtha+" "+heighta);
 // keep the existing HTML as there is a button that is needed
-document.getElementById("graphWrapper").innerHTML=document.getElemen
-tById("graphWrapper").innerHTML+'<div class="h-75 w-75"><svg
-width="'+widtha+'" height="'+heighta+'" id="svg1"></svg></div>'
+document.getElementById("graphWrapper").innerHTML=document.getElementById("graphWrapper").innerHTML+'<div class="h-75 w-75"><svg width="'+widtha+'" height="'+heighta+'" id="svg1"></svg></div>'
 }
