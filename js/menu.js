9f8af18fb7514394830255330c718d2ec2ce5833
Xintong Guan <xintong052499@gmail.com>
2022-03-09 10:12:02 +0000

    Added menu.js holding functions will be called in the menu

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+
+function bestCondition() {
+	alert("This is function showing a list of assets that have best condition in at least one report "+ arguments.callee.name); 
+}
+
+function menu3() {
+	alert("Under channel 'Menu 2', menu is called by: "+ arguments.callee.name); 
+}
+
+function menu4() {
+	alert("Under channel 'Menu 2', menu is called by: "+ arguments.callee.name); 
+}
+
+function menu7() {
+	alert("Under channel 'Menu 5', menu is called by: "+ arguments.callee.name); 
+}
+
+
+function menu9() {
+	alert("Under channel 'Menu 8', menu is called by: "+ arguments.callee.name); 
+}
+
+function menu10() {
+	alert("Under channel 'Menu 8', menu is called by: "+ arguments.callee.name); 
+}
+
+
+function menu12() {
+	alert("Under channel 'Menu 11', menu is called by: "+ arguments.callee.name); 
+}
+function menu13() {
+	alert("Under channel 'Menu 11', menu is called by: "+ arguments.callee.name); 
+}
+function menu15() {
+	alert("Under channel 'Menu 14', menu is called by: "+ arguments.callee.name); 
+}
+function menu16() {
+	alert("Under channel 'Menu 14', menu is called by: "+ arguments.callee.name); 
+}
