70a31ff172cb4321e5a0f5ec1d36aa43716a6971
Xintong Guan <xintong052499@gmail.com>
2022-03-09 10:29:27 +0000

    Updated all function with an alert and referenced them in the bootstrap.html

--- js/menu.js
+++ js/menu.js
@@ -1,39 +1,38 @@
 
 function bestCondition() {
-	alert("This is function showing a list of assets that have best condition in at least one report "+ arguments.callee.name); 
+	alert("This is the function showing a list of assets that have best condition in at least one report: "+ arguments.callee.name); 
 }
 
-function menu3() {
-	alert("Under channel 'Menu 2', menu is called by: "+ arguments.callee.name); 
+function dailyReportRate() {
+	alert("This is the function showing the bar graph of daily reporting rate for the past week: "+ arguments.callee.name); 
 }
 
-function menu4() {
-	alert("Under channel 'Menu 2', menu is called by: "+ arguments.callee.name); 
+function help() {
+	alert("This is the function: "+ arguments.callee.name); 
 }
 
-function menu7() {
-	alert("Under channel 'Menu 5', menu is called by: "+ arguments.callee.name); 
-}
-
-
-function menu9() {
-	alert("Under channel 'Menu 8', menu is called by: "+ arguments.callee.name); 
-}
-
-function menu10() {
-	alert("Under channel 'Menu 8', menu is called by: "+ arguments.callee.name); 
+function userRank() {
+	alert("This is the function: "+ arguments.callee.name); 
 }
 
 
-function menu12() {
-	alert("Under channel 'Menu 11', menu is called by: "+ arguments.callee.name); 
+function Show5Asset() {
+	alert("This is the function showing the 5 assets closest to the user’s current location: "+ arguments.callee.name); 
 }
-function menu13() {
-	alert("Under channel 'Menu 11', menu is called by: "+ arguments.callee.name); 
+
+function Remove5Asset() {
+	alert("This is the function removing the 5 assets closest to the user’s current location: "+ arguments.callee.name); 
 }
-function menu15() {
-	alert("Under channel 'Menu 14', menu is called by: "+ arguments.callee.name); 
+
+function Show5Report() {
+	alert("This is the function showing the last 5 reports created by the user: "+ arguments.callee.name); 
 }
-function menu16() {
-	alert("Under channel 'Menu 14', menu is called by: "+ arguments.callee.name); 
+function Remove5Report() {
+	alert("This is the function removing the last 5 reports created by the user: "+ arguments.callee.name); 
+}
+function ShowRate() {
+	alert("This is the function showing assets and that user hasn't rated in the last 3 days : "+ arguments.callee.name); 
+}
+function RemoveRate() {
+	alert("This is the function removing assets and that user hasn't rated in the last 3 days : "+ arguments.callee.name); 
 }
